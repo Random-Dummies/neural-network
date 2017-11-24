dbbb9f2d15a2b86ae41ef2b4a56666de131aff50
Rishabh Srivastava <f2013674@goa.bits-pilani.ac.in>
2017-11-24 20:28:48 +0530

    Create nested-json-handler.py

---
+++ nested-json-handler.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+import json
+def getScores(payload):
+    scoreDict = {"skills":0,"exp":0,"Crev":0,"CErev":0,"NeoApi":0}
+    # JSON requires double-quotes, not single-quotes.
+    payload = json.loads(payload)
+    print(payload)
+    for doc in payload['records']:
+        scoreDict[doc['type']] = doc['score']
+        print(doc['score'], doc['type'])
+    scoreList = []
+    scoreList.append(scoreDict["skills"])
+    scoreList.append(scoreDict["experience"])
+    scoreList.append(scoreDict["CompanyReview"])
+    scoreList.append(scoreDict["CEOReview"])
+    scoreList.append(scoreDict["NeoApi"])
+    return scoreList
